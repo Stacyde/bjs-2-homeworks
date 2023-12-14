82aa8ab5892973f63fab0a71a6fec0b133092414
Stacyde <degtich@mail.ru>
2023-12-14 11:15:17 +0400

    added task3

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,17 @@
 function compareArrays(arr1, arr2) {
-  
+    if (arr1.length === arr2.length) {
+		return arr1.every((element, i) => element === arr2[i]);
+	}
+	return false;
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    let result = users.filter(user => user.gender === gender).map(old => old.age).reduce((usAge, old, index, arr) => {
+        usAge += old;
+        if (index === arr.length - 1) {
+            return usAge / arr.length;
+        }
+        return usAge;
+      }, 0);
+      return result;
 }
